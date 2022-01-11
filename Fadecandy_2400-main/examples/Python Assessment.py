005d7eb302eee48391db101da32114991df893b4
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-01-11 21:31:32 +0000

    added some basic animations
    basic animations to experiment and to formulate more complicated animations

--- Fadecandy_2400-main/examples/Python Assessment.py
+++ Fadecandy_2400-main/examples/Python Assessment.py
@@ -8,7 +8,30 @@ client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-value = input("Welcome to the show! What would you like to see first? \n Choose from the options below:")
+#value = input("Welcome to the show! What would you like to see first? \n Choose from the options below:")
 
-print("What show would you like to see first? \n Answer:", value)
+#print("What show would you like to see first? \n Answer:", value)
 
+
+#for led in range(60):
+#    leds[led] = (255,0,0)
+#    time.sleep(.1)
+#   client.put_pixels(leds)
+
+#led = 0
+#while led<60: #scroll all rows at the same time
+#    for rows in range(3): #first three rows left to right
+#        leds[led + rows*60] = (0,0,255)
+#    for rows in range(3,6): #last three rows reversed (right to left)
+#        leds[59-led + rows*60] = (50,50,255)
+#    client.put_pixels(leds)
+#    time.sleep(.1)
+#    led = led + 1
+
+led = 0
+while led<60:
+    for rows in range(6):
+        leds[led + rows*60] = (255,0,0) #add one led in front
+    client.put_pixels(leds)
+    time.sleep(.1)
+    led = led + 1
