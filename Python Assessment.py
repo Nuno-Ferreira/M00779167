94f2ad1f31b88c750532db633e0836fbc7647d15
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-01-11 19:38:48 +0000

    Created assessment file
    with basic code for importing the necessaries libraries and to communicate with the client.

---
+++ Python Assessment.py
@@ -0,0 +1,10 @@
+import opc
+import time
+import random
+
+leds = [(255,255,255)]*360 #white for every pixel
+
+client = opc.Client('localhost:7890')
+client.put_pixels(leds)
+client.put_pixels(leds)
+
