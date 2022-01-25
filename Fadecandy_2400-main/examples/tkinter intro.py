091a13170b5aa543a2311802c48de4ae184b0526
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-01-25 15:57:49 +0000

    Create tkinter intro.py

---
+++ Fadecandy_2400-main/examples/tkinter intro.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+for row in range(3):
+    for col in range(3):
+        frame = tk.Frame(master = window, relief = tk.RAISED, borderwidth = 1)
+        frame.grid(row = row, column = col, padx = 5, pady = 5)
+        label = tk.Label(master = frame, text = f"{row}, {col}")
+        label.pack()
+
+
+window.mainloop()
